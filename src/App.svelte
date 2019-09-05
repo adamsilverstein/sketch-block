1a9573ba4bc19cb719b1a2fbc11cdbeb77008417
Adam Silverstein <adam@10up.com>
2019-09-05 17:43:23 -0600

    i can draw

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,93 @@
 <script>
 	export let name;
-</script>
 
+	let m = { x: 0, y: 0 };
+	let mouseIsDown = false;
+	let blockGrid = [];
+	let width = 100;
+	let height = 100;
+	let blockWidth = window.innerWidth / width;
+	let blockHeight = window.innerHeight / height;
+
+	// Reset the Block grid
+	const resetBlockGrid = () => {
+		for ( let x = 0; x < height; x++ ) {
+			blockGrid[ x ] = [];
+			for ( let y = 0; y < width; y++ ) {
+				blockGrid[ x ][ y ] = '.';
+			}
+		}
+	}
+	resetBlockGrid();
+
+	function handleMousemove(event) {
+		if ( ! mouseIsDown ) {
+			return;
+		}
+		m.x = event.clientX;
+		m.y = event.clientY;
+		let blockY = Math.floor( m.x / blockWidth );
+		let blockX = Math.floor( m.y / blockHeight );
+		console.log( blockX, blockY );
+		blockGrid[ blockX ] = blockGrid[ blockX ] || [];
+		blockGrid[ blockX ][ blockY ] = 'X';
+	}
+
+	function handleMouseDown() {
+		console.log( 'handleMouseDown' );
+		mouseIsDown = true;
+	}
+
+	function handleMouseUp() {
+		console.log( 'handleMouseUp' );
+		mouseIsDown = false;
+	}
+
+
+	function styles(props) { getStyles(props); }
+
+
+	const getStyles = () => (
+	`		background-color: #eee;
+			width: ${ Math.floor( window.innerWidth / document.getElementsByClass('blockgrid').clientWidth ) }px;
+			height: ${ Math.floor( window.innerHeight /  document.getElementsByClass('blockgrid').clientHeight ) }px;
+	`
+	);
+</script>
 <style>
-	h1 {
-		color: purple;
+	div {
+		width: 100%;
+		height:100%;
+	}
+	td {
+		list-style-type: none;
+		font-family: "Lucida Console", Monaco, monospace;
+	}
+	::selection {
+		background-color: transparent;
 	}
 </style>
 
-<h1>Hello {name}!</h1>
+<div
+	on:mousemove={ handleMousemove }
+	on:mousedown={ handleMouseDown }
+	on:mouseup={ handleMouseUp }
+	class="blockgrid"
+>
+	<table >
+	{ #each blockGrid as gridrow }
+		<tr >
+			{ #each gridrow as item }
+				<td style={getStyles()}>
+					{ item }
+				</td>
+			{ /each }
+
+ 		</tr>
+	{ /each }
+	</table>
+<button on:click={ resetBlockGrid }>
+	Clear
+</button>
+
+</div>
